bbf9ec599448c392f887d72578bc137150a15f19
ads-b791 <ads-b@i.ua>
2023-02-24 20:03:57 +0200

    перед рефакторингом

--- js/main.js
+++ js/main.js
@@ -2,7 +2,31 @@ const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
+let tasks = [];
 
+if (localStorage.getItem('tasks')) {
+	tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+tasks.forEach(function (task) {
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+	const taskHTML = `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+	<span class="${cssClass}">${task.text}</span>
+	<div class="task-item__buttons">
+		<button type="button" data-action="done" class="btn-action">
+			<img src="./img/tick.svg" alt="Done" width="18" height="18">
+		</button>
+		<button type="button" data-action="delete" class="btn-action">
+			<img src="./img/cross.svg" alt="Done" width="18" height="18">
+		</button>
+	</div>
+</li>`
+
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
+});
+
+checkEmptyList();
 
 form.addEventListener('submit', addTask);
 
@@ -15,8 +39,20 @@ function addTask(e) {
 
 	const taskText = taskInput.value;
 
-	const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
-	<span class="task-title">${taskText}</span>
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		done: false,
+	}
+
+	tasks.push(newTask);
+
+	saveToLocalStorage();
+
+	const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
+
+	const taskHTML = `<li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+	<span class="${cssClass}">${newTask.text}</span>
 	<div class="task-item__buttons">
 		<button type="button" data-action="done" class="btn-action">
 			<img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -31,27 +67,80 @@ function addTask(e) {
 	taskInput.value = "";
 	taskInput.focus();
 
-	if (tasksList.children.length > 1) {
-		emptyList.classList.add('none');
-	}
+	//if (tasksList.children.length > 1) {
+	//	emptyList.classList.add('none');
+	//}
+	checkEmptyList();
 }
+
 
 function deleteTask(e) {
 
 	if (e.target.dataset.action !== 'delete') return;
 
 	const parentNode = e.target.closest('.list-group-item');
-	parentNode.remove();
-	if (tasksList.children.length === 1) {
-		emptyList.classList.remove('none');
-	}
 
+	const id = Number(parentNode.id);
+
+	//const index = tasks.findIndex(function (task) {
+	//if (task.id === id) {
+	//	return true
+	//}
+	//	return task.id === id;
+	//});
+	//tasks.splice(index, 1);
+
+	tasks = tasks.filter(function (task) {
+		//if (task.id === id) {
+		//	return false
+		//} else {
+		//	return true
+		//}
+		return task.id !== id;
+	})
+
+	saveToLocalStorage();
+
+	parentNode.remove();
+	//if (tasksList.children.length === 1) {
+	//	emptyList.classList.remove('none');
+	//}
+	checkEmptyList();
 }
 
 function doneTask(e) {
 
 	if (e.target.dataset.action !== 'done') return;
 	const parentNode = e.target.closest('.list-group-item');
+
+	const id = Number(parentNode.id);
+
+	const task = tasks.find(function (task) {
+		return task.id === id;
+	});
+
+	task.done = !task.done
+
+	saveToLocalStorage();
+
 	const taskTitle = parentNode.querySelector('.task-title');
 	taskTitle.classList.toggle('task-title--done');
+}
+
+function checkEmptyList() {
+	if (tasks.length === 0) {
+		const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+		<img src="./img/empty-2.jpg" alt="Empty" width="48" class="mt-3">
+		<div class="empty-list__title">Список дел пуст</div>
+	</li>`;
+		tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+	}
+	if (tasks.length > 0) {
+		const emptyListEl = document.querySelector('#emptyList');
+		emptyListEl ? emptyListEl.remove() : null;
+	}
+}
+
+function saveToLocalStorage() {
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
